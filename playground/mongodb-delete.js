96caefc916e505293d162fa8d5c48a90de30ed66
Guy Pender <guy.pender@me.com>
2018-03-02 22:15:57 +0000

    added delete methods

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,11 +8,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) =>{
         console.log('Connected to MongoDb Server');
     }
     // delete many
-    db.collection.deleteMany({text:'Something to do'}).then((result)=>{
-        console.log(result);
-    });
+    // db.collection('Todos').deleteMany({text:'Something to do'}).then((result)=>{
+    //     console.log(result);
+    // });
     // delete one
+    // db.collection('Todos').deleteOne({text:'Walk the dog'}).then((result)=>{
+    //     console.log(result);
+    // });
     // find one and delete
-
-    //db.close();
+    // db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+    //     console.log(result);
+    // });
+    db.collection('Users').findOneAndDelete({_id:123}).then((result) => {
+        console.log(JSON.stringify(result,undefined,2));
+    });
+    db.close();
 });
