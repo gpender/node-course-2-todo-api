01903753471265919ff1cd8c1cef77b61b9dfdcd
Guy Pender <guy.pender@me.com>
2018-03-03 17:44:33 +0000

    Added tests for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,15 +34,15 @@ app.get('/todos',(req,res)=>{
 app.get('/todos/:id',(req,res)=>{
     var id = req.params.id;
     if(!ObjectID.isValid(id)){
-        return res.status(400).send('Invalid Id ' + id);
+        return res.status(404).send('Invalid Id ' + id);
     }
     Todo.findById(id).then((todo)=>{
         if(!todo){
-            return res.status(400).send();
+            return res.status(404).send();
         }   
         res.send({todo});
     }).catch((e)=>{
-        res.status(400).send();
+        res.status(404).send();
     });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,19 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 //const {app} = require('./../server');
 const app = require('./../server').app;
 const Todo = require('./../models/todo').Todo;
 
-const todos = [{text:'todo1'},{text:'todo2'}];
+const todos = [
+    {
+        _id:new ObjectID(),
+        text:'todo1'},
+    {
+        _id:new ObjectID(),
+        text:'todo2'
+    }];
 
 beforeEach((done) =>{
     Todo.remove({}).then(()=> {
@@ -60,6 +68,34 @@ describe('GET /todos',()=>{
         .expect((res)=>{
             expect(res.body.todos.length).toBe(2);
         })
+        .end(done);
+    });
+});
+
+
+describe('GET /todos/:id',()=>{
+    it('should return todo doc',(done)=>{
+        console.log(todos[0]._id.toHexString());
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+    it('should return a 404 if todo not found',(done)=>{
+        var duffId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${duffId}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return a 404 for non object Ids',(done)=>{
+        var duffId = 1238;
+        request(app)
+        .get(`/todos/${duffId}`)
+        .expect(404)
         .end(done);
     });
 });
